f1058fbafbeec0008a1624c20c0afedd4646c891
sjbgit <sjb_strat@hotmail.com>
2015-07-06 17:45:33 -0500

    mongoose mapreduce working

--- goose3.js
+++ goose3.js
@@ -1,6 +1,9 @@
 /**
  * Created by sbunke on 7/6/2015.
  */
+/**
+ * Created by sbunke on 7/6/2015.
+ */
 //http://mongoosejs.com/docs/api.html#model_Model.mapReduce
 //https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 var mongoose = require('mongoose');
@@ -15,6 +18,27 @@ var aggregateSchema = new mongoose.Schema({
 var AggregateSchema = mongoose.model('AggregateSchema', aggregateSchema);
 
 
+var map = function(){
+    emit(this.identifier, this.count);
+}
+
+// reduce function
+var reduce =  function (gender, count) {
+    return Array.sum(count);
+}
+
+
+var  o = {
+    map: map,
+    reduce: reduce,
+}
+
+AggregateSchema.mapReduce(o, function(err, results, stats) {
+    console.log(results);
+    console.log('map reduce took %d ms', stats.processtime);
+});
+
+/*
 var as = new AggregateSchema( { identifier: 'mongoose1', count: 22});
 
 as.save(function(err, thor) {
@@ -36,3 +60,4 @@ AggregateSchema.findOne({ _id: '559b016c898b29c8a123a4e3' }, function(err, thor)
     })
 
 });
+    */
